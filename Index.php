684530b4d0543854e5b7266467c2078c834d5cc7
Mitrix44 <127190716+Mitrix44@users.noreply.github.com>
2024-01-06 16:33:19 +0100

    leger correctif

--- Index.php
+++ Index.php
@@ -7,7 +7,9 @@ require_once "./class/classSquare.php";
 require_once "./class/classRectangle.php";
 
 $imageEditor = new ImageEditor;
-$imageEditor->addNewGroup([new ImageEditor(), new Square(10, 10, 10), new Rectangle(50, 20, 5, 4)]);
+$compoundGraphic = new compoundGraphic;
+$compoundGraphic->add(new Circle(20, 20, 20));
+$imageEditor->addNewGroup([$compoundGraphic, new Square(10, 10, 10), new Rectangle(50, 20, 5, 4)]);
 
 $imageEditor->draw();
 $imageEditor->move(5, 5);
